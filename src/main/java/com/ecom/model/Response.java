500256daa2c4e7b2a42278cff1214246d8b418ae
Lakshit <69980302+Lakshit-Gupta@users.noreply.github.com>
2024-04-19 15:45:58 +0530

    Add files via upload

---
+++ src/main/java/com/ecom/model/Response.java
@@ -0,0 +1,30 @@
+package com.ecom.model;
+
+
+/**
+ *
+ * @author rahul
+ *
+ */
+public class Response {
+
+    private int statusCode;
+    private RazorPay razorPay;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+    public RazorPay getRazorPay() {
+        return razorPay;
+    }
+
+    public void setRazorPay(RazorPay razorPay) {
+        this.razorPay = razorPay;
+    }
+
+
+
+}
