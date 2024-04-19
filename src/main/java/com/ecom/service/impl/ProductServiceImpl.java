734597fc3089aa54ea1a7809e22535ca8139c86a
Lakshit <69980302+Lakshit-Gupta@users.noreply.github.com>
2024-04-19 15:48:21 +0530

    Create ProductServiceImpl.java

---
+++ src/main/java/com/ecom/service/impl/ProductServiceImpl.java
@@ -0,0 +1,49 @@
+package com.ecom.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import com.ecom.model.Product;
+import com.ecom.repository.ProductRepository;
+import com.ecom.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	private final ProductRepository productRepository;
+
+	@Autowired
+	public ProductServiceImpl(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	@Override
+	public Product getProductById(int productId) {
+		return productRepository.findById(productId).orElse(null);
+	}
+
+	@Override
+	public Product saveProduct(Product product) {
+		return productRepository.save(product);
+	}
+
+	@Override
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+
+	@Override
+	public Boolean deleteProduct(Integer id) {
+		Product product = productRepository.findById(id).orElse(null);
+
+		if (!ObjectUtils.isEmpty(product)) {
+			productRepository.delete(product);
+			return true;
+		}
+		return false;
+	}
+
+}
